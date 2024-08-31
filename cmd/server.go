412daefa2913368c52d4cece3eaca73579068631
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-08-31 15:39:34 +0530

    adds a server cmd

---
+++ cmd/server.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/bsach64/goback/server"
+	"github.com/spf13/cobra"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "starts a server",
+	Long:  "starts a server",
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Listen()
+	},
+}
