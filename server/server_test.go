14b0ce955b7fdcce0d149f17fbd24cbff652e689
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-09-02 12:30:29 +0530

    fixes formatting

--- server/server_test.go
+++ server/server_test.go
@@ -19,19 +19,17 @@ func TestKey(t *testing.T) {
 	}
 }
 
-
 // Test the listen function
-func TestListen(t *testing.T){
+func TestListen(t *testing.T) {
 	s := New("0.0.0.0", "../private/id_rsa", 2022)
-    go Listen(s)
-    time.Sleep(1*time.Second)
-    conn, err := net.Dial("tcp", "127.0.0.1:2022")
-    if err != nil {
+	go Listen(s)
+	time.Sleep(1 * time.Second)
+	conn, err := net.Dial("tcp", "127.0.0.1:2022")
+	if err != nil {
 		t.Fatalf("Failed to connect to the SFTP server: %v", err)
 	}
 	conn.Close()
 
 	t.Log("SFTP server started and accepted connections successfully")
 
-    
 }
