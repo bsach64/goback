2fadd998303998ea7db01f868430d55a02f5e1ec
Om Diwan <omdiwan111.od@gmail.com>
2024-09-22 02:45:55 +0530

    server cmd modified

--- cmd/server.go
+++ cmd/server.go
@@ -1,23 +1,27 @@
 package cmd
 
 import (
-	"log"
-	"github.com/charmbracelet/huh"
 	"github.com/bsach64/goback/server"
+	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
+	"log"
 )
+
 var mainOptions string
 var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "starts a server",
+	Long:  "starts a server",
 	Run: func(cmd *cobra.Command, args []string) {
 		form := huh.NewForm(
 			huh.NewGroup(
 				huh.NewSelect[string]().
 					Title("Choose an option:").
 					Options(
-						huh.NewOption("Listen","Listen"),
-						huh.NewOption("Log","Log"),
-						huh.NewOption("Reconstruct","Reconstruct"),
-						huh.NewOption("Exit","Exit"),
+						huh.NewOption("Listen", "Listen"),
+						huh.NewOption("Log", "Log"),
+						huh.NewOption("Reconstruct", "Reconstruct"),
+						huh.NewOption("Exit", "Exit"),
 					).
 					Value(&mainOptions),
 			),
@@ -27,7 +31,7 @@ var serverCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-		
+
 		switch mainOptions {
 		case "Listen":
 			s := server.New("0.0.0.0", "private/id_rsa", 2022)
