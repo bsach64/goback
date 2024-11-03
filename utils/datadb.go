f90a709cbd6d601fb248a366c5968e4bea5c1431
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-11-03 22:27:25 +0530

    feat: create sqlite3 db connection

---
+++ utils/datadb.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/ncruces/go-sqlite3/driver"
+	_ "github.com/ncruces/go-sqlite3/embed"
+)
+
+func CreateDBConn(filename string) error {
+	db, err := sql.Open("sqlite3", fmt.Sprintf("file:%s", filename))
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
